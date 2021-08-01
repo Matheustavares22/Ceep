0e80d1e992cceba3d2774aae2a95a56ff0401d08
Matheus Tavares <matheus.tav.nun@gmail.com>
2021-08-01 10:26:42 -0300

    Inflate the menu_form_note_save.xml

--- app/src/main/java/alura/com/br/ceep/ui/activity/FormNoteActivity.java
+++ app/src/main/java/alura/com/br/ceep/ui/activity/FormNoteActivity.java
@@ -1,8 +1,9 @@
 package alura.com.br.ceep.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import alura.com.br.ceep.R;
 
@@ -12,5 +13,11 @@ public class FormNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_note);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_form_note_save, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
